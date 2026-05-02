a414948f6ffe07c8a8e206d3a7a102dabdda1db0
van06-0419 <zhiyuan.wang@yandex.ru>
2026-05-02 08:40:21 +0700

    Remove AddMultipleAircraft method from AirlineService
    Removed the AddMultipleAircraft method from AirlineService class.

--- AirlineFactory/AirlineDLL/include/AirlineService.h
+++ AirlineFactory/AirlineDLL/include/AirlineService.h
@@ -7,6 +7,5 @@
 class AIRLINE_API AirlineService {
 public:
     void AddAircraft(Airline& airline, AircraftFactory& factory); // Добавить один самолёт: создать через фабрику и добавить в авиакомпанию
-    void AddMultipleAircraft(Airline& airline, std::vector<AircraftFactory*>& factories); // Добавить несколько самолётов: создать несколько самолётов через список фабрик
     const std::vector<AbstractAircraft*>& GetFleet(const Airline& airline) const;
 };
