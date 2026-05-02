6bb808373cbc42652453b8d19056713148e4d4e2
van06-0419 <zhiyuan.wang@yandex.ru>
2026-05-02 08:03:12 +0700

    Update AircraftPrinter.h

--- AirlineFactory/AirlineDLL/include/AircraftPrinter.h
+++ AirlineFactory/AirlineDLL/include/AircraftPrinter.h
@@ -5,9 +5,9 @@
 #include "PassengerJet.h"
 #include "CargoPlane.h"
 
+// Класс для вывода информации о самолётах (сервисный класс)
 class AIRLINE_API AircraftPrinter {
 public:
     static void Print(const AbstractAircraft* aircraft);
     static void PrintFleet(const std::vector<AbstractAircraft*>& fleet, const std::string& airlineName);
-    static void PrintSeparator();
 };
