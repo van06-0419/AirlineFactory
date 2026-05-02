e501d7f410f64bb51621ca92a2cb3ecc4d80df76
van06-0419 <zhiyuan.wang@yandex.ru>
2026-05-02 08:40:58 +0700

    Refactor AirlineService by removing redundant methods
    Removed the AddMultipleAircraft method and cleaned up the AddAircraft method.

--- AirlineFactory/AirlineDLL/src/AirlineService.cpp
+++ AirlineFactory/AirlineDLL/src/AirlineService.cpp
@@ -5,22 +5,6 @@ void AirlineService::AddAircraft(Airline& airline, AircraftFactory& factory) {
     airline.AddAircraft(factory.Create());
 }
 
-// добавление нескольких самолётов
-void AirlineService::AddMultipleAircraft(Airline& airline, std::vector<AircraftFactory*>& factories) {
-    // Получить количество фабрик 
-    int count = factories.size();
-    for (int i = 0; i < count; i++) {
-        // Получить i-ый указатель на фабрику
-        AircraftFactory* factory = factories[i];
-
-        // Проверка безопасности: использовать только если фабрика не nullptr
-        if (factory != nullptr) {
-            // Создать самолёт и добавить в флот
-            airline.AddAircraft(factory->Create());
-        }
-    }
-}
-
 const std::vector<AbstractAircraft*>& AirlineService::GetFleet(const Airline& airline) const {
     return airline.GetFleet();
 }
